9985a28f3df066205fc39364dc7d209600f7d8b4
RaiFarhan0 <167308448+RaiFarhan0@users.noreply.github.com>
2026-04-07 17:20:11 +0500

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 3000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          three: ['three', '@react-three/fiber', '@react-three/drei'],
+          physics: ['@react-three/rapier'],
+          gsap: ['gsap', '@gsap/react']
+        }
+      }
+    }
+  }
+});
